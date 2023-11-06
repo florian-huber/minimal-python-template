079d5b786a08ff067879985680208848b16f4df8
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2023-11-06 14:43:37 +0100

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,26 +1,34 @@
 # Note: cookiecutter first makes the main level directory using
 # directory_name from cookiecutter.json before running this hook
 
-import json
+import re
 import sys
 
-# Load the context that cookiecutter has generated from the user inputs
-with open('cookiecutter.json') as f:
-    context = json.load(f)
+# Function to validate the package name
+def is_valid_package_name(name):
+    return re.match("^[a-zA-Z_][a-zA-Z0-9_]*$", name)
 
-# Update the context based on the user inputs
-context.update({
-    "package_name": context['package_name'].lower().replace(" ", "_").replace("-", "_"),
-    "directory_name": context['directory_name'].lower().replace(" ", "-"),
-    "full_name": context['full_name'].replace('"', '\\"'),
-    "repository": "https://github.com/" + context['github_organization'] + "/" + context['directory_name'].lower().replace(" ", "-"),
-    "package_short_description": context['package_short_description'].replace('"', '\\"'),
-})
+# Access the cookiecutter dictionary which contains all the input variables
+package_name = '{{ cookiecutter.package_name }}'
+directory_name = '{{ cookiecutter.directory_name }}'
+full_name = '{{ cookiecutter.full_name }}'
+github_organization = '{{ cookiecutter.github_organization }}'
+package_short_description = '{{ cookiecutter.package_short_description }}'
 
-# Overwrite the cookiecutter.json file with the new context
-with open('cookiecutter.json', 'w') as f:
-    json.dump(context, f)
+# Perform your manipulations
+package_name = package_name.lower().replace(" ", "_").replace("-", "_")
+directory_name = directory_name.lower().replace(" ", "-")
+full_name = full_name.replace('"', '\\"')
+repository = "https://github.com/{}/{}".format(github_organization, directory_name)
+package_short_description = package_short_description.replace('"', '\\"')
 
-# Optional: print out the updated context or any messages
-print("Updated context:", context)
+# Example of using the validation function
+if not is_valid_package_name(package_name):
+    print('ERROR: The package name "{}" is not a valid Python package name.'.format(package_name))
+    # Exit with an error code
+    sys.exit(1)
 
+# You would typically set these variables in the context without writing back to cookiecutter.json.
+# The values would be available in the template files directly using the cookiecutter object.
+
+print('Everything is fine with the inputs.')
