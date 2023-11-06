9d6ee7fa7b5070fa8ebb4b53dd3f9acf561ab891
Florian Huber <florian.huber@hs-duesseldorf.de>
2023-11-06 14:56:00 +0100

    black linting

--- {{cookiecutter.directory_name}}/tests/test_my_module.py
+++ {{cookiecutter.directory_name}}/tests/test_my_module.py
@@ -5,19 +5,19 @@ from {{cookiecutter.package_name}}.my_module import hello
 
 
 def test_hello():
-    assert hello('Alice') == 'Hello Alice!'
+    assert hello("Alice") == "Hello Alice!"
 
 
 def test_hello_with_error():
     with pytest.raises(ValueError) as excinfo:
-        hello('nobody')
-    assert 'Can not say hello to nobody' in str(excinfo.value)
+        hello("nobody")
+    assert "Can not say hello to nobody" in str(excinfo.value)
 
 
 @pytest.fixture
 def some_name():
-    return 'Jane Smith'
+    return "Jane Smith"
 
 
 def test_hello_with_fixture(some_name):
-    assert hello(some_name) == 'Hello Jane Smith!'
+    assert hello(some_name) == "Hello Jane Smith!"
