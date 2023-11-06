93411d9921a059c19f69e29651c78ad6695c5e2d
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2023-11-06 14:41:29 +0100

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,19 +1,26 @@
 # Note: cookiecutter first makes the main level directory using
 # directory_name from cookiecutter.json before running this hook
 
-{
-    {
-        cookiecutter.update(
-            {
-                "package_name": cookiecutter.package_name.lower().replace(" ", "_").replace("-", "_"),
-                "directory_name": cookiecutter.directory_name.lower().replace(" ", "-"),
-                "full_name": cookiecutter.full_name.replace('"', '\\"'),
-                "repository": "https://github.com/"
-                + cookiecutter.github_organization
-                + "/"
-                + cookiecutter.directory_name.lower().replace(" ", "-"),
-                "package_short_description": cookiecutter.package_short_description.replace('"', '\\"'),
-            }
-        )
-    }
-}
+import json
+import sys
+
+# Load the context that cookiecutter has generated from the user inputs
+with open('cookiecutter.json') as f:
+    context = json.load(f)
+
+# Update the context based on the user inputs
+context.update({
+    "package_name": context['package_name'].lower().replace(" ", "_").replace("-", "_"),
+    "directory_name": context['directory_name'].lower().replace(" ", "-"),
+    "full_name": context['full_name'].replace('"', '\\"'),
+    "repository": "https://github.com/" + context['github_organization'] + "/" + context['directory_name'].lower().replace(" ", "-"),
+    "package_short_description": context['package_short_description'].replace('"', '\\"'),
+})
+
+# Overwrite the cookiecutter.json file with the new context
+with open('cookiecutter.json', 'w') as f:
+    json.dump(context, f)
+
+# Optional: print out the updated context or any messages
+print("Updated context:", context)
+
